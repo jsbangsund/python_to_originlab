570a0b4120f99fe66ccc8d8b5b236c9454a7dfd7
jsbangsund <jsb@umn.edu>
2019-07-29 15:57:42 -0500

    Update numpy_to_origin.py

--- numpy_to_origin.py
+++ numpy_to_origin.py
@@ -38,19 +38,26 @@ def numpy_to_origin(
         origin.NewProject
         # Wait for origin to compile
         origin.Execute("sec -poc 3.5")
-        time.sleep(1)
+        time.sleep(2)
         
-    # Check if workbook exists. If not create a workbook page
+    # Check if workbook exists. If not create a new workbook page with this name
+    layer_idx=None
     if origin.WorksheetPages(workbook_name) is None:
+        print(workbook_name)
+        print(origin.WorksheetPages(workbook_name))
         workbook_name = origin.CreatePage(2, workbook_name , 'Origin') # 2 for workbook
+        # Use Sheet1 if workbook is newly made
+        layer_idx=0
     # get workbook instance from name
     wb = origin.WorksheetPages(workbook_name)
-    wb.Layers.Add() # Add a worksheet
-    #then find the last worksheet to modify (to avoid overwriting other data)
-    i = 0
-    while not (wb.Layers(i) is None):
-        i+=1
-    ws=wb.Layers(i-1) # Get worksheet instance, index starts at 0.
+    if layer_idx is None:
+        wb.Layers.Add() # Add a worksheet
+        #then find the last worksheet to modify (to avoid overwriting other data)
+        i = 0
+        while not (wb.Layers(i) is None):
+            i+=1
+        layer_idx = i-1
+    ws=wb.Layers(layer_idx) # Get worksheet instance, index starts at 0.
     ws.Name=worksheet_name # Set worksheet name
     # For now, assume only x and y data for each line (ignore error data)
     ws.Cols=data_array.shape[column_axis] # Set number of columns in worksheet
@@ -67,13 +74,71 @@ def numpy_to_origin(
             # Set column data type to ( 0=Y, 3=X , ?=X error, ?=Y error)
             col.Type=types[col_idx]
         origin.PutWorksheet('['+wb.Name+']'+ws.Name, np.float64(data_array[col_idx,:]).tolist(), 0, col_idx) # start row, start col
-    #origin.PutWorksheet('['+wb.Name+']'+ws.Name, np.float64(data_array).tolist(), 0, col_idx) # start row, start col
+    #origin.PutWorksheet('['+wb.Name+']'+ws.Name, np.float64(data_array).T.tolist(), 0, col_idx) # start row, start col
     if not user_defined is None:
         # User Param Rows
         for idx,param in enumerate(user_defined):
             ws.Execute('wks.UserParam' + str(idx+1) + '=1; wks.UserParam' + str(idx+1) + '$="' + param[0] + '";')
             ws.Execute('col(1)[' + param[0] + ']$="' + param[1] + '";')
+        origin.Execute('wks.col1.width=10;')
+    return origin,wb,ws
+    
+def  createGraph_multiwks(origin,graphName,template,templatePath,worksheets,x_cols,y_cols,LineOrSym,
+                       origin_version=2018):
+    # worksheets must be a list of worksheets
+    # template is the full path and template filename
+    # Each worksheet must be formatted identically
+    # x_col, y_col, and LineOrSym should be same length
+    # Create graph page and object
+    templateFullPath=os.path.join(templatePath,template)
+    # Create graph if doesn't already exist
+    graphLayer = origin.FindGraphLayer(graphName)
+    if graphLayer is None:
+        graphName = origin.CreatePage(3, graphName, templateFullPath)
+        # Find the graph layer
+        graphLayer = origin.FindGraphLayer(graphName)
 
-        origin.Execute('wks.col2.width=7;')
-    return origin
+    # Get dataplot collection from the graph layer
+    dataPlots = graphLayer.DataPlots
+
+    # Add data column by column to the graph
+    # loop over worksheets within column loops so that data from same column
+    # can be grouped. E.g. all PL data is in same column and will be grouped.
+    for ci,x_col in enumerate(x_cols):
+        for wi,worksheet in enumerate(worksheets):
+            # Create a data range
+            # Tested only on origin 2016 and 2018
+            if origin_version<=2016:
+                dr = origin.NewDataRange # Make a new datarange
+            elif origin_version>2016:
+                dr = origin.NewDataRange()
+            
+            # Add data to data range
+            #                  worksheet, start row, start col, end row (-1=last), end col
+            dr.Add('X', worksheet, 0 , x_col,       -1, x_col)
+            dr.Add('Y', worksheet, 0 , y_cols[ci], -1, y_cols[ci])
+            # Add data plot to graph layer
+            # 200 -- line
+            # 201 -- symbol
+            # 202 -- symbol+line
+            # If specified, plot symbol. By default, plot line
+            if LineOrSym[ci] in ['Sym','Symbol','Symbols']:
+                dataPlots.Add(dr, 201)
+            elif LineOrSym[ci] == 'Line+Sym':
+                dataPlots.Add(dr,202)
+            else:
+                dataPlots.Add(dr, 200)
+
+        # Group each column (This allows colors to be automatically incremented
+        # and a single legend entry to be created for all the data sets with
+        # the same legend entry)
+        BeginIndex = ci*len(worksheets) + 1;
+        EndIndex = BeginIndex + len(worksheets) - 1;
+        graphLayer.Execute('layer -g ' + str(BeginIndex) + ' ' + str(EndIndex) + ';')
+    # Rescales axes
+    #Rescale type: 1 = manual, 2 = normal, 3 = auto, 4 = fixed from, and 5 = fixed to.
+    #graphLayer.invoke('Execute','layer.axis.rescale=3');
+    graphLayer.Execute('Rescale')
+    graphLayer.Execute('legend -r')
+    return graphName
     # To exit, call origin.Exit()
